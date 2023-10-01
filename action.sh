e0c66d8db7ec9dd5fab1a0bc4d887c664cd4849b
lubinacourec <79009964+lubinacourec@users.noreply.github.com>
2023-10-01 12:34:59 +0100

    Update action.sh

--- action.sh
+++ action.sh
@@ -12,7 +12,7 @@ if [ -z "${MIRROR}" ]; then
   echo 'Please specify a github repo to mirror to ("user/repo")'
   exit 1
 fi
-
+echo "mirroring ${REMOTE} to github.com/${MIRROR}"
 git clone --bare "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${MIRROR}.git" . || exit 1
 git config --global --add safe.directory /github/workspace || exit 1
 git remote add --mirror=fetch mirror "${REMOTE}" || exit 1
